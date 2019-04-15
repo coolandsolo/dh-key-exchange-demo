0f59a0bd93f77d9f3a6cf6c4d6e593cc921e4c7d
Solomon Afiemo <solomon.afiemo@fobbitllc.com>
2019-04-15 01:16:20 -0400

    Implemented shared numbers page

--- src/layout/screens/SharedNumbers.js
+++ src/layout/screens/SharedNumbers.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  head: {
+    fontWeight: 200,
+  },
+};
 
 class SharedNumbers extends Component {
   render() {
+    const { classes, appState } = this.props;
+    const { prime, generator } = appState;
     return (
       <div>
-        Agreed upon shared numbers<br />
-        Prime: <strong>0</strong><br />
-        Generator: <strong>0</strong>
+        <Typography className={classes.head} variant="h3">Both parties have agreed on shared numbers</Typography><br /><br />
+        Prime: <strong>{prime}</strong><br />
+        Generator: <strong>{generator}</strong>
         <br /><br />
         <Button variant="contained" color="primary" className="next">Next</Button>
       </div>
@@ -15,4 +26,8 @@ class SharedNumbers extends Component {
   }
 }
 
-export default SharedNumbers;
+SharedNumbers.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SharedNumbers);
